15270c578850b80568bd8eb6dad9db4220d78617
0debianlaw0 <96630708+0debianlaw0@users.noreply.github.com>
2023-05-15 05:59:30 +0200

    Arreglada la mina

--- Assets/Mina.cs
+++ Assets/Mina.cs
@@ -13,13 +13,11 @@ public class Mina : MonoBehaviour
     }
     void FixedUpdate()
     {
-
         RaycastHit hit;
-        
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask) && used == false)
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.left), out hit, Mathf.Infinity, layerMask) && used == false)
         {
             used = true;
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * hit.distance, Color.yellow);
             Debug.Log("Did Hit");
             particleSystem.Play();
             healthManager.DamagePlayer(30);
@@ -27,7 +25,7 @@ public class Mina : MonoBehaviour
         }
         else
         {
-            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.left) * 1000, Color.white);
             //Debug.Log("Did not Hit");
         }
     }
