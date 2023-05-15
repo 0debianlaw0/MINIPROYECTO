b8da4950d9c084103b765c5884b47496387c9849
0debianlaw0 <96630708+0debianlaw0@users.noreply.github.com>
2023-05-15 05:02:04 +0200

    MENU AND LVL2 IMPLEMENTED

--- Assets/enemy.cs
+++ Assets/enemy.cs
@@ -10,12 +10,16 @@ public class enemy : MonoBehaviour
     int vida = 100;
     public Animator animator;
     public Collider collider;
+    public ParticleSystem particleSystem;
+    public HealthManager healthManager;
+    bool isIn;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
         collider = GetComponent<Collider>();
+        particleSystem = GetComponentInChildren<ParticleSystem>();
     }
 
     private void Update()
@@ -23,6 +27,7 @@ public class enemy : MonoBehaviour
         if (colD == true)
         {
             agent.destination = goal.position;
+            TenSeconds();
         }
         if (vida <= 0)
         {
@@ -38,14 +43,41 @@ public class enemy : MonoBehaviour
 
         }
     }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isIn = true;
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            isIn = false;
+
+        }
+    }
 
     public void Damage()
     {
         vida = vida - 100;
     }
 
+    public void DamagePlayer()
+    {
+        if (isIn == true)
+        {
+            healthManager.currentHealth -= 10;
+        }
+    }
+
     IEnumerator TenSeconds()
     {
         yield return new WaitForSeconds(10f);
+        particleSystem.Play();
+        Destroy(gameObject, 0.5f);
     }
 }
